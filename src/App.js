e968b327afd4f69311791bba85741baabae5d24d
metulboss <metulromel@gmail.com>
2022-07-09 23:27:50 +0600

    card-create

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ function App() {
 const bg={
   backgroundColor:"purple",
   color:"white",
-  padding:"15px"
+  padding:"15px",
+  textAlign:"center"
 }
 
   return (
     <div className="App">
      <h1 style={bg}>React - Component</h1>
+<div className='card'>
+     <h1 className='card-title'>React Crad</h1>
+     <p className='card-des'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis et delectus commodi eaque expedita libero, harum excepturi praesentium laborum vitae iusto quia atque sequi cupiditate amet recusandae. Exercitationem, deserunt est.</p>
+     <p className='card-footer'>footer</p>
+</div>
     </div>
   );
 }
